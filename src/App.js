ff9b972cad5386c0813dd0a697f331c1bd9214b3
0x4bel <45129317+magyariabel@users.noreply.github.com>
2024-08-30 15:15:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import KPISelector from './components/KPISelector';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>KPI Tool Recommender for Positive Energy Districts</h1>
+      </header>
+      <main>
+        <KPISelector />
+      </main>
+    </div>
+  );
+}
+
+export default App;
