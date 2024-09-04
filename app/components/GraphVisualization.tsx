b8445141b82f6260084b3ec16fa1ad390926634e
magyariabel <magyari.abel@gmail.com>
2024-09-04 16:22:33 +0100

    new_app3

--- app/components/GraphVisualization.tsx
+++ app/components/GraphVisualization.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import ForceGraph2D from 'react-force-graph-2d'
 
 export default function GraphVisualization({ data }) {
@@ -9,7 +9,7 @@ export default function GraphVisualization({ data }) {
   useEffect(() => {
     if (graphRef.current) {
       graphRef.current.d3Force('charge').strength(-100)
-      graphRef.current.d3Force('link').distance(100)
+      graphRef.current.d3Force('link').distance(50)
     }
   }, [])
 
@@ -17,10 +17,13 @@ export default function GraphVisualization({ data }) {
     <ForceGraph2D
       ref={graphRef}
       graphData={data}
-      nodeLabel="id"
+      nodeLabel="name"
       nodeAutoColorBy="group"
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
+      linkCurvature={0.25}
+      width={600}
+      height={400}
     />
   )
 }
